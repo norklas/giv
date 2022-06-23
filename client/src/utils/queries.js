7d75242a8d7ae9e019b368214b91d6e4f9523f67
Nick Melanson <nickm890@gmail.com>
2022-06-23 19:25:44 -0400

    user and me queries added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,65 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      causes {
+        _id
+        title
+        description
+        url
+        location
+        category
+        points
+        comments {
+          _id
+          body
+          createdAt
+          username
+        }
+        medals {
+          _id
+          body
+          username
+          createdAt
+        }
+      }
+      points
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($id: ID!) {
+    user(_id: $id) {
+      __id
+      username
+      email
+      causes {
+        _id
+        title
+        description
+        url
+        location
+        category
+        points
+        comments {
+          _id
+          body
+          createdAt
+          username
+        }
+        medals {
+          _id
+          body
+          username
+          createdAt
+        }
+      }
+      points
+    }
+  }
+`;
