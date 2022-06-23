e834f45353926f36031f0696f5fd011a49f53d6d
Nick Melanson <nickm890@gmail.com>
2022-06-23 19:35:46 -0400

    added all queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -75,6 +75,35 @@ export const QUERY_USER = gql`
   }
 `;
 
+export const QUERY_USERS = gql`
+  query users {
+    _id
+    username
+    email
+    causes {
+      _id
+      title
+      description
+      url
+      location
+      comments {
+        _id
+        body
+        username
+        createdAt
+      }
+      medals {
+        _id
+        body
+        username
+        createdAt
+      }
+      category
+      points
+    }
+  }
+`;
+
 export const QUERY_USER_BASIC = gql`
   query user($id: ID!) {
     user(_id: $id) {
@@ -82,6 +111,58 @@ export const QUERY_USER_BASIC = gql`
       username
       email
       points
+    }
+  }
+`;
+
+export const QUERY_CAUSE = gql`
+  query cause($id: ID!) {
+    cause(_id: $id) {
+      cause {
+        _id
+        title
+        description
+        url
+        location
+        category
+        points
+        comments {
+          _id
+          body
+          createdAt
+          username
+        }
+        medals {
+          _id
+          body
+          username
+          createdAt
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_CAUSES = gql`
+  query causes {
+    _id
+    title
+    description
+    url
+    location
+    category
+    points
+    comments {
+      _id
+      body
+      createdAt
+      username
+    }
+    medals {
+      _id
+      body
+      username
+      createdAt
     }
   }
 `;
