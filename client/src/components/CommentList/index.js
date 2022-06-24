121789d6da93abf3d3dfc2ee0132f9ad1fcd347f
Erica <erica.trenholm48@gmail.com>
2022-06-23 20:57:54 -0400

    comment components

---
+++ client/src/components/CommentList/index.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+const CommentList = () => {
+    const sampleComments = [
+        {
+            username: "testuser",
+            body: "Morbi eleifend nibh eu.",
+            createdAt: "June 23, 2022",
+        },
+        {
+            username: "testuser2",
+            body: "Nulla eget porta erat.",
+            createdAt: "June 23, 2022",
+        },
+        {
+            username: "testuser3",
+            body: "Sed arcu nibh, dapibus sit amet imperdiet a, sollicitudin ac risus.",
+            createdAt: "June 23, 2022",
+        }
+    ]
+
+    return (
+        <div>
+            {sampleComments.map((comment) => (
+                <div class="single-comment">
+                    <h4>{comment.username}</h4>
+                    <p class="date">{comment.createdAt}</p>
+                    <p>{comment.body}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default CommentList;
