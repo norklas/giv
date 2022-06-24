63086e4b3fff76da3a016f6477dcdbe316aa7cd7
Erica <erica.trenholm48@gmail.com>
2022-06-23 20:02:19 -0400

    footer and loginModal components

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,34 @@
 import logo from "../../assets/givlogo.svg"
+import { useState } from 'react'
+import LoginModal from "../LoginModal"
 
 const Header = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen)
+    }
+
     return (
         <div>
+            {isModalOpen && (<LoginModal onClose={toggleModal}/>)}
             <header>
                 <div class="header-flex global-container">
                     <a href="/">
                         <img src={logo} alt="giv logo" />
                     </a>
+
+                    <div class="search-bar">
+                        <input class="search" type="text" placeholder="Search.." />
+                        <button type="submit" class="search-button">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+
                     <nav>
                         <ul>
-                            <li>About</li>
-                            <li>Sign in</li>
+                            <li>Sign up</li>
+                            <li onClick={() => toggleModal()}>Log in</li>
                             <li><a href="" class="nav-btn">Create a Cause</a></li>
                         </ul>
                     </nav>
