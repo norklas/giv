5bd8e276e6058c9c7add8dac415906fad749d26f
noreikam <noreikam1@gmail.com>
2022-06-26 15:03:58 -0400

    added cause data to upper cause card

--- client/src/pages/SingleCause.js
+++ client/src/pages/SingleCause.js
@@ -12,19 +12,20 @@ import { useState } from 'react';
 const SingleCause = () => {
     const { causeId: causeParam } = useParams();
     console.log("causeId: " + causeParam)
-    // const { loading, data } = useQuery(QUERY_CAUSE, {
-    //     variables: { id: causeParam }
-    // });
-    // const causeData = data?.cause || {};
-    // console.log(causeData);
+    const { loading, data } = useQuery(QUERY_CAUSE, {
+        variables: { id: causeParam }
+    });
+    const causeData = data?.cause || {};
+    console.log(causeData);
+    console.log(causeData.title);
 
     // useState for new comment
     const[commentState, setCommentState] = useState({ body: '' })
     const [addComment, { error } ] = useMutation(ADD_COMMENT);
 
     // get username of logged in user for comment submit
-    const { loading, data } = useQuery(QUERY_ME);
-    const username = data?.me.username || '';
+    const { data: userData } = useQuery(QUERY_ME);
+    const username = userData?.me.username || '';
 
     // handle comment change
     const handleComment = (event) => {
@@ -57,16 +58,15 @@ const SingleCause = () => {
                 <button class="category-btn category-btn-single-cause disaster-relief">Disaster Relief</button>
                 <div class="point-count">
                     <FontAwesomeIcon icon={faStar} className='icon'/>
-                    <div class="bottom-text">7391 Points</div>
+                    <div class="bottom-text">{causeData.points}</div>
                     </div>
                 </div>
             <div class="single-card-bottom">
-                <h3>Support Example Charity</h3>
-                <p class="date">June 16, 2022</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum eu dui id finibus. Nulla fringilla quis tellus et ultricies. Quisque aliquam lacinia mi in tincidunt. Sed vitae tincidunt sapien, non accumsan augue. Aenean id scelerisque risus. Ut interdum imperdiet nulla at laoreet. Maecenas eu lectus quam. Aenean tincidunt sodales ante vitae varius. Duis sagittis lorem quis ex posuere aliquet. Sed nisi mauris, placerat ac suscipit id, laoreet vitae sem. Nullam non diam ultricies, dictum purus eget, egestas leo.</p>
-                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ut elit eu nibh aliquam imperdiet. Aenean semper aliquet sem, sit amet finibus ligula blandit quis. Duis vel risus sodales, condimentum lorem eu, efficitur ante. Morbi feugiat tristique eros, eget convallis augue mattis sed. Sed ultrices libero vitae lacus tincidunt, at fermentum ipsum viverra. Aliquam pharetra eget velit ac tristique. Cras sed lorem nulla. Praesent nec sollicitudin dui. Vivamus feugiat eu justo eget pellentesque. Sed a dolor vitae leo porttitor tincidunt eget eu justo. Donec vehicula consectetur nunc nec lacinia. Ut a urna ut nulla posuere tempus eget eget massa. Vestibulum ullamcorper felis sit amet lacus pellentesque pellentesque. Curabitur sit amet neque vitae libero pharetra fringilla id ut tortor. Nam ac libero tellus.</p>
-                <div class="author">Erica Trenholm</div>
-                <button class="web-btn">Visit website</button>
+                <h3>{causeData.title}</h3>
+                <p class="date">{causeData.createdAt}</p>
+                <p>{causeData.description}</p>
+                <div class="author">{causeData.location}</div>
+                <button class="web-btn"><a href={causeData.url}>Visit website</a></button>
             </div>
         </div>
 
