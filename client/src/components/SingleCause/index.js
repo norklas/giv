331209e2f941a8ca5d836b6f4bb80ea836b62ecf
Nick Melanson <nickm890@gmail.com>
2022-06-28 15:40:24 -0400

    added loading back into singlecause to fix features breaking

--- client/src/components/SingleCause/index.js
+++ client/src/components/SingleCause/index.js
@@ -3,31 +3,33 @@ import { faMessage, faStar } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import DonateModal from "../DonateModal";
-import CommentList from '../CommentList';
+import CommentList from "../CommentList";
 
 import goldmedal from "../../assets/medal-01.svg";
 import silvermedal from "../../assets/medal-02.svg";
 import bronzemedal from "../../assets/medal-03.svg";
 
 const SingleCause = (props) => {
-
-  const { cause } = props;
-  let bronzes = 0
-  let silvers = 0
-  let golds = 0
-  let platinums = 0
-  const medalArr = cause.medals
-  for(let i = 0; i < medalArr.length; i ++){
-    if(medalArr[i].body === 'Bronze'){
-      bronzes ++
-    }if(medalArr[i].body === 'Silver'){
-      silvers ++
-    }if(medalArr[i].body === 'Gold'){
-      golds ++
-    }else{platinums++}
+  const { cause, loading } = props;
+  let bronzes = 0;
+  let silvers = 0;
+  let golds = 0;
+  let platinums = 0;
+  const medalArr = cause.medals;
+  for (let i = 0; i < medalArr.length; i++) {
+    if (medalArr[i].body === "Bronze") {
+      bronzes++;
+    }
+    if (medalArr[i].body === "Silver") {
+      silvers++;
+    }
+    if (medalArr[i].body === "Gold") {
+      golds++;
+    } else {
+      platinums++;
+    }
   }
-  console.log(bronzes, silvers, golds, platinums)
-
+  console.log(bronzes, silvers, golds, platinums);
 
   const [donateModalOpen, setDonateModalOpen] = useState(false);
   const [donationNumber, setDonationNumber] = useState(0);
@@ -44,7 +46,7 @@ const SingleCause = (props) => {
   const [commentListOpen, setCommentListOpen] = useState(false);
   const toggleCommentList = () => {
     setCommentListOpen(!commentListOpen);
-  }
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -61,42 +63,57 @@ const SingleCause = (props) => {
         />
       )}
       <div className="card-top">
-        
         {(() => {
-        if(bronzes){
-          return(
-            <div>
-         <img src={bronzemedal} className="bronzemedal" alt="Gold Medal" />
-        <p>Bronze: {bronzes}</p>
-        </div>)
-          }  
+          if (bronzes) {
+            return (
+              <div>
+                <img
+                  src={bronzemedal}
+                  className="bronzemedal"
+                  alt="Gold Medal"
+                />
+                <p>Bronze: {bronzes}</p>
+              </div>
+            );
+          }
         })()}
         {(() => {
-        if(silvers){
-          return(
-            <div>
-         <img src={silvermedal} className="silvermedal" alt="Silver Medal" />
-        <p>Silver: {silvers}</p>
-        </div>)
-          }  
+          if (silvers) {
+            return (
+              <div>
+                <img
+                  src={silvermedal}
+                  className="silvermedal"
+                  alt="Silver Medal"
+                />
+                <p>Silver: {silvers}</p>
+              </div>
+            );
+          }
         })()}
         {(() => {
-        if(golds){
-          return(
-            <div>
-         <img src={goldmedal} className="goldmedal" alt="Gold Medal" />
-        <p>Gold: {golds}</p>
-        </div>)
-          }  
+          if (golds) {
+            return (
+              <div>
+                <img src={goldmedal} className="goldmedal" alt="Gold Medal" />
+                <p>Gold: {golds}</p>
+              </div>
+            );
+          }
         })()}
         {(() => {
-        if(platinums){
-          return(
-            <div>
-         <img src={silvermedal} className="platmedal" alt="Platinum Medal" />
-        <p>Platinum: {platinums}</p>
-        </div>)
-          }  
+          if (platinums) {
+            return (
+              <div>
+                <img
+                  src={silvermedal}
+                  className="platmedal"
+                  alt="Platinum Medal"
+                />
+                <p>Platinum: {platinums}</p>
+              </div>
+            );
+          }
         })()}
         <h3>{cause.title}</h3>
         <p className="date">June 16, 2022</p>
@@ -122,18 +139,15 @@ const SingleCause = (props) => {
           </div>
         </div>
         <div className="comment-count">
-          <FontAwesomeIcon 
+          <FontAwesomeIcon
             icon={faMessage}
             className="icon"
             onClick={() => toggleCommentList()}
           />
           <div className="bottom-text">{cause.comments.length} Comments</div>
-          
         </div>
-        
       </div>
-      {commentListOpen && (
-            <CommentList comments = {cause.comments} />)}
+      {commentListOpen && <CommentList comments={cause.comments} />}
     </div>
   );
 };
