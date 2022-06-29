d6edbed1312b2814c6817f3e97ebac95316828ef
Nick Melanson <nickm890@gmail.com>
2022-06-29 16:46:39 -0400

    reorganized code, renamed all class to className

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,12 @@
 import CauseList from "../components/CauseList";
 
 const Home = () => {
-    return (            
-        <div class="home">
-            <h2>Discover your local causes</h2>
-            <CauseList />
-        </div>      
-    )
-}
+  return (
+    <div className="home">
+      <h2>Discover your local causes</h2>
+      <CauseList />
+    </div>
+  );
+};
 
 export default Home;
